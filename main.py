c18130d922b944501e0e4ef1c8d186235f870459
raj-odc <83627491+raj-odc@users.noreply.github.com>
2025-02-01 13:39:31 +0530

    changes in the main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-import streamlit as st
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain import LLMChain
+from langchain import PromptTemplate
 
+import streamlit as st
 import os
 
-os.environ['GOOGLE_API_KEY']  = "AIzaSyBVKO64abIwVDTdvJZHKJy5CqNNlps9hnM"
-# Using Google Models (Gemini Pro)
-from langchain_google_genai import ChatGoogleGenerativeAI
-
-from langchain import PromptTemplate
+os.environ['GOOGLE_API_KEY'] = st.secrets['GOOGLE_API_KEY']
 
 # Create prompt template for generating tweets
 
@@ -18,20 +17,21 @@ tweet_prompt = PromptTemplate(template = tweet_template, input_variables = ['num
 gemini_model = ChatGoogleGenerativeAI(model = "gemini-1.5-flash-latest")
 
 
-from langchain import LLMChain
-
-
 # Create LLM chain using the prompt template and model
 tweet_chain = tweet_prompt | gemini_model
 
 
+import streamlit as st
 
 st.header("Tweet Generator")
+
 st.subheader("Generate tweets using Generative AI")
 
 topic = st.text_input("Topic")
-number = st.number_input("Number of tweets", min_value=1,max_value=10, value=1, step=1)
+
+number = st.number_input("Number of tweets", min_value = 1, max_value = 10, value = 1, step = 1)
 
 if st.button("Generate"):
-    tweets = tweet_chain.invoke({"number": number, "topic": topic})
+    tweets = tweet_chain.invoke({"number" : number, "topic" : topic})
     st.write(tweets.content)
+    
